4e034df03a7e2ba6967c80f300de81d8c7a70f1d
Nicholas <enenbee@tx.rr.com>
2021-06-21 18:07:55 -0500

    Add files via upload

---
+++ scrchex/morejs.js
@@ -0,0 +1,32 @@
+class MoreJS {
+    getInfo() {
+        return {
+            "id": "MoreJS",
+            "name": "MoreJS",
+            "blocks": [
+                {
+                    "opcode":"testblock",
+                    "blockType": "reporter",
+                    "text": "TestBlock [num1] ^ [num2]",
+                    "arguments": {
+                        "num1": {
+                            "type":"number",
+                            "defaultValue":"2"
+                        },
+                        "num2": {
+                            "type":"number",
+                            "defaultValue":"8"
+                        }
+                    }
+                }
+            ],
+            "menus": {
+
+            }
+        }
+    }
+    testblock(num1,num2) {
+        return num1**num2
+    }
+}
+Scratch.extensions.register(new MoreJS())
