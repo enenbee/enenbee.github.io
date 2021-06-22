acdcaddab8a07f2dc07a9db77d0c096732272231
Nicholas <enenbee@tx.rr.com>
2021-06-22 08:52:44 -0500

    Update morejs.js

--- scrchex/morejs.js
+++ scrchex/morejs.js
@@ -1,6 +1,6 @@
-const jsdom = require('jsdom')
-const dom = new jsdom.JSDOM("")
-const jquery = require('jquery')(dom.window)
+//const jsdom = require('jsdom')
+//const dom = new jsdom.JSDOM("")
+//const jquery = require('jquery')(dom.window)
 
 class MoreJS {
     getInfo() {
