4ddc4dd512d595a25065ff8bf000e8f1a1f65867
Nicholas <enenbee@tx.rr.com>
2021-07-02 21:26:33 -0500

    Add files via upload

---
+++ other/base100.js
@@ -0,0 +1,9 @@
+_base100code="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~`!@#$%^&*()-_=+[{]}\|;:'\",<.>/?€ƒ„…†‡"
+
+function base100encode(number){
+    var n=""+number
+    if(n.length%2==1) n="0"+n
+
+    return n.match(/.{1,2}/g).map((v)=>_base100code[v/1]).join("")
+
+}
