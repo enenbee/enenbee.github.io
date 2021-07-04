ba07616da0a55de08d023c357657409d0373b296
Nicholas <enenbee@tx.rr.com>
2021-07-04 12:44:23 -0500

    Update zenjs.js

--- zen-js/zenjs.js
+++ zen-js/zenjs.js
@@ -75,8 +75,9 @@ my=0
 
 b.onmousemove=(e)=>{
     r=c.getBoundingClientRect()
-    mx=e.x-r.x,
-    my=e.y-r.y
+    var scale=r.width/c.width
+    mx=(e.x-r.x)/scale,
+    my=(e.y-r.y)/scale
 }
 
 b.onmousedown=()=>{mdown=true}
