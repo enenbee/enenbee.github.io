c5568ee9183ff4eee623147d03caf3c91f14409c
Nicholas <enenbee@tx.rr.com>
2021-07-08 06:37:16 -0500

    Update worker.js

--- other/fragment-cpu/worker.js
+++ other/fragment-cpu/worker.js
@@ -1,12 +1,10 @@
 onmessage=(e)=>{
     
-    var fragment=e.data[2]
-    
     var out=[]
     
     for(var i=0;i<e.data[1];i++){
     
-        out.push(fragment(e.data[0],i))
+        out.push(e.data[2](e.data[0],i))
     
     }
     
