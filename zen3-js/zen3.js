778641575c144aebfa1ca57cd3ac7ea06cbbfd1a
Nicholas <enenbee@tx.rr.com>
2021-07-28 09:33:42 -0500

    Update zen3.js

--- zen3-js/zen3.js
+++ zen3-js/zen3.js
@@ -38,3 +38,42 @@ function litObject(shape,material,rec){
     return out
 
 }
+
+//lights
+
+function ambience(_color,_bright){
+    
+    var color=_color
+    if(color==undefined)color=0xffffff
+
+    var bright=_bright
+    if(bright==undefined)bright=0.1
+    
+    return new THREE.AmbientLight(color,bright)
+    
+}
+function pointLight(pos,_color,_bright,_near,_far){
+    
+    var color=_color
+    if(color==undefined)color=0xffffff
+
+    var bright=_bright
+    if(bright==undefined)bright=0.8
+
+    var near=_near
+    if(near==undefined)near=0.1
+
+    var far=_far
+    if(far==undefined)far=25
+    
+    var out=new THREE.PointLight(color,bright)
+    out.position.set(pos[0],pos[1],pos[2])
+    
+    out.castShadow=true
+    out.shadow.camera.near=near
+    out.shadow.camera.far=far
+    
+    return out
+    
+}
+
