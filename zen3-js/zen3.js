8ee8d876a9f4932e5fc563b2d22c767d21c19f39
Nicholas <enenbee@tx.rr.com>
2021-07-28 09:21:08 -0500

    Add files via upload

---
+++ zen3-js/zen3.js
@@ -0,0 +1,31 @@
+
+//this requires three.js in some form
+
+res=[1920,1080]
+
+function makeScene(){
+    return new THREE.Scene()
+}
+function makeCamera(_fov,_near,_far){
+
+    var fov=_fov
+    if(fov==undefined)fov=90
+
+    var near=_near
+    if(near==undefined)near=0.1
+
+    var far=_far
+    if(far==undefined)far=1000
+    
+    return new THREE.PerspectiveCamera(fov,res[0]/res[1],near,far)
+}
+
+function litObject(shape,material,rec){
+    var out=new THREE.Mesh(shape,material)
+
+    out.receiveShadow=(rec==undefined?true:rec)
+    out.castShadow=true
+
+    return out
+
+}
