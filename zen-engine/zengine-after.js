1e7ce5cfc90b8882a995e229285a74f1e26d568d
Nicholas <enenbee@tx.rr.com>
2021-08-26 16:46:05 -0500

    Add files via upload

---
+++ zen-engine/zengine-after.js
@@ -0,0 +1,20 @@
+
+c.width=ratio[0]
+c.height=ratio[1]
+b.appendChild(c)
+
+_f = window.requestAnimationFrame(()=>{
+    _frame++
+
+    ctx.resetTransform()
+    ctx.translate(-_camera[0],-_camera[1])
+    ctx.scale(_detail,_detail)
+
+    c.width=ratio[0]*_detail
+    c.height=ratio[1]*_detail
+
+    d_clear(clearcolor)
+    Frame()
+
+    changed={}
+})
