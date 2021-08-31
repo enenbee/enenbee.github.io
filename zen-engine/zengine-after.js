1f57785e56feeac959c1232de1ca2a57a6b1fc26
Nicholas <enenbee@tx.rr.com>
2021-08-31 16:24:18 -0500

    Changable FPS

--- zen-engine/zengine-after.js
+++ zen-engine/zengine-after.js
@@ -3,14 +3,4 @@ c.width=ratio[0]
 c.height=ratio[1]
 b.appendChild(c)
 
-_f = setInterval(()=>{
-    useCamera(ctx,_camera)
-
-    c.width=ratio[0]*_detail
-    c.height=ratio[1]*_detail
-
-    d_clear(clearcolor)
-    Frame()
-
-    changed={}
-},1000/60)
+setFPS(60)
