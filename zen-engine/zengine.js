3e6f5d3c2d8e5266f2e0bc5971e469adac17d228
Nicholas <enenbee@tx.rr.com>
2021-09-05 21:54:56 -0500

    Fixed a bug
    Fixed a bug that I kept having in my games. i hate the bug enough that I removed it from the main engine

--- zen-engine/zengine.js
+++ zen-engine/zengine.js
@@ -382,9 +382,9 @@ function d_image(img,x,y,attt){
     cctx.rotate(att.rot||0)
 
     if(att.height==undefined){
-        cctx.drawImage(img,-(att.rotx||0),-(att.roty||0))
+        cctx.drawImage(img,-(att.rotx/_detail||0),-(att.roty/_detail||0),img.width/_detail,img.height/_detail)
     } else {
-        cctx.drawImage(img,-(att.rotx||0),-(att.roty||0),att.width,att.height)
+        cctx.drawImage(img,-(att.rotx/_detail||0),-(att.roty/_detail||0),att.width/_detail,att.height/_detail)
     }
 
     cctx.setTransform(tmp)
